ac2841728680f5549e1dd8fd37b457c0772884d4
qxia <qxqinxia@outlook.com>
2023-09-07 22:02:02 +0200

    tododo

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -8,13 +8,37 @@ function App() {
   function handleSubmit(e) {
     e.preventDefault()
 
-    setTodos([
-      ...todos,
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
       {
         id:crypto.randomUUID(), title: newItem, completed: false
       },
-    ])
+      ]
+    })
+
+    setNewItem("")
   }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          // todo.completed = completed
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id != id)
+    })
+  }
+
+  console.log(todos)
 
   return (
     <>
@@ -32,20 +56,20 @@ function App() {
       </form>
       <h1 className='header'>Todo List</h1>
       <ul className='list'>
-        <li>
+        {todos.length === 0 && "No Todos"}
+        {todos.map(todo => {
+          return (
+          <li key={todo.id}>
           <label>
-            <input type="checkbox" />
-            Item 1
+            <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+            {todo.title}
           </label>
-          <button className='btn btn-danger'>Delete</button>
+          <button 
+            onClick={() => deleteTodo(todo.id)}
+            className='btn btn-danger'>Delete</button>
         </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className='btn btn-danger'>Delete</button>
-        </li>
+          )
+        })}
       </ul>
     </>
   )
